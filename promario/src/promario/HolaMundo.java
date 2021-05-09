97f1ba8e39aeaa426b1e2a87c2111cf7ffda1781
MarioTorres44 <mario.torres.cid@gmail.com>
2021-05-09 22:09:14 +0200

    Cuarto cambio primer usuario

--- promario/src/promario/HolaMundo.java
+++ promario/src/promario/HolaMundo.java
@@ -7,6 +7,7 @@ public class HolaMundo {
 			System.out.println("HolaMundo1");
 			System.out.println("HolaMundo2");
 			System.out.println("HolaMundo3 como Mario");
+			System.out.println("Caurto cambio primer usuario");
 		}
 
 }
